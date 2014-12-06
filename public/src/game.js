0268cbe56ae202b482999093a38f98b2a1b7c81a
William Tan <williamjefftan@gmail.com>
2014-12-06 13:35:55 -0500

    coded base game with sample car

--- public/src/game.js
+++ public/src/game.js
@@ -1,16 +1,31 @@
-var Q = Quintus()
-    .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI")
-    .setup({ maximize: true })
-    .controls().touch();
+window.addEventListener("load",function() {
 
-Q.Sprite.extend("Car",{ //Create car sprite
+    var Q = Quintus({ imagePath: "assets/"})
+        .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI")
+        .setup({ maximize: true })
+        .controls().touch();
 
-    init: function(c) {
+        Q.gravityY = 0;
+        Q.gravityX = 0;
 
-        this._super(c, {
-            sheet: "car",
-            x: 410,
-            y: 90
-        });
-    }
+    Q.Sprite.extend("Car",{ //Create car sprite
+
+        init: function(c) {
+
+            this._super(c, {
+                asset: "car",
+                x: 410,
+                y: 90
+            });
+        }
+    });
+
+    Q.scene("level1", function(stage) {
+        var car = stage.insert(new Q.Car());
+        stage.add("viewport").follow(car);
+    });
+
+    Q.load("car.png", function() {
+        Q.stageScene("level1")
+    });
 });
