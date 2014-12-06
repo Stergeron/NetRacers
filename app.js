bbe5d29f10ee349ef5df3f8d527ac709cefe7822
Max <themaxaxis@gmail.com>
2014-12-06 13:31:04 -0500

    it works

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-  res.render('/index.html');
-});
 
 var port = process.env.PORT || 8888;
 
 var server = app.listen(port);
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+  socket.on("down", function(id){
+    io.emit("clientdown", id);
+  });
+  socket.on("up", function(id){
+    io.emit("clientup", id);
+  });
+});
