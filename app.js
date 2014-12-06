6d45f3b46829bd628bbba822a6ae6a75c1b7c145
Max <themaxaxis@gmail.com>
2014-12-06 18:14:47 -0500

    Lobby System is complete i guess
    meh

--- app.js
+++ app.js
@@ -10,11 +10,75 @@ var server = app.listen(port);
 
 var io = require('socket.io')(server);
 
-io.on('connection', function(socket){
-  socket.on("down", function(id){
-    io.emit("clientdown", id);
+var lobbies = [{
+  name: "poo",
+  members: ["john", "jim"],
+  open: true
+}, {
+  name: "noo",
+  members: ["nothn"],
+  open: true
+}];
+
+var players = [];
+
+io.on('connection', function(socket) {
+  var player;
+  var lobby;
+  socket.on("joinGame", function(nm, cb){
+    if(players.indexOf(nm) < 0){
+      cb(nm);
+      player = nm;
+    }
+    else {
+      cb("REJECT");
+    }
   });
-  socket.on("up", function(id){
-    io.emit("clientup", id);
+  socket.on("joinLobby", function(us) {
+    lobbies[findForIndex(lobbies, "name", us.lob)].members.push(us.player);
+    if (lobbies[findForIndex(lobbies, "name", us.lob)].members.length >= 4) {
+      lobbies[findForIndex(lobbies, "name", us.lob)].open = false;
+    }
+    player.lobby = us.lob;
+    io.emit("updateLobby", findBy(lobbies, "name", us.lob));
+  });
+  socket.on("leaveLobby", function(lob) {
+    leaveLobby(lob);
+  });
+  socket.on("request", function(nm, cb) {
+    cb(lobbies);
+  });
+  socket.on('disconnect', function () {
+    if(lobby !== undefined){
+      leaveLobby({name: lobby, player: player});
+    }
+    if(player !== undefined){
+      players.splice(players.indexOf(player), 1);
+    }
   });
 });
+
+var leaveLobby = function(lob) {
+  var index = findForIndex(lobbies, "name", lob.name);
+  lobbies[index].members.splice(lobbies[index].members.indexOf(lob.player), 1);
+  if (findBy(lobbies, "name", lob.name).members.length < 1) {
+    lobbies.splice(findForIndex(lobbies, "name", lob.name), 1);
+    io.emit("removeLobby", lob.name);
+  }
+};
+
+var findBy = function(arr, identifier, name) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i][identifier] == name) {
+      return arr[i];
+    }
+  }
+};
+
+var findForIndex = function(arr, identifier, name) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i][identifier] == name) {
+      return i;
+    }
+  }
+};
--- public/src/testing.js
+++ public/src/testing.js
@@ -1,40 +1,81 @@
-var socket = io();
+(function() {
+  var app = angular.module("matchmaking", []);
 
-var ppl = {
-  1: {
-    key: false
-  },
-  2: {
-    key: false
-  },
-  3: {
-    key: false
-  },
-  4: {
-    key: false
-  }
-};
+  app.controller("Player", function(g, socket) {
+    this.l = g.context;
+    var l = g.context;
+    this.joinLobby = function(lob) {
+      this.l.currentLobby = findBy(this.l.lobbies, "name", lob);
+      socket.emit("joinLobby", {
+        player: this.l.playerName,
+        lob: lob
+      });
+      this.l.view = "lobby";
+    };
+    socket.on("updateLobby", function(lob) {
+      if (findBy(l.lobbies, "name", lob.name) !== undefined) {
+        l.lobbies[findForIndex(l.lobbies, "name", lob.name)] = lob;
+      } else {
+        l.lobbies.push(lob);
+      }
+    });
+    socket.on("removeLobby", function(lob) {
+      l.lobbies.splice(findForIndex(l.lobbies, "name", lob.name), 1);
+    });
+    g.init();
+  });
 
-var myPerson = prompt("id?");
+  app.service("g", function(socket) {
+    this.context = {
+      view: "lobbies",
+      currentLobby: {},
+      lobbies: [],
+      playerName: "untitled"
+    };
+    var ctx = this;
+    this.init = function() {
+      socket.emit("request", "", function(dat) {
+        ctx.context.lobbies = dat;
+      });
+    };
+  });
+  var findBy = function(arr, identifier, name) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i][identifier] == name) {
+        return arr[i];
+      }
+    }
+  };
 
-window.addEventListener('keydown', function (e) {
-  if(e.keyCode == 71){
-    socket.emit("down", myPerson);
-  }
-}, false);
+  var findForIndex = function(arr, identifier, name) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i][identifier] == name) {
+        return i;
+      }
+    }
+  };
+  app.factory('socket', function($rootScope) {
+    var socket = io();
+    return {
+      on: function(eventName, callback) {
+        socket.on(eventName, function() {
+          var args = arguments;
+          $rootScope.$apply(function() {
+            callback.apply(socket, args);
+          });
+        });
+      },
+      emit: function(eventName, data, callback) {
+        socket.emit(eventName, data, function() {
+          var args = arguments;
+          $rootScope.$apply(function() {
+            if (callback) {
+              callback.apply(socket, args);
+            }
+          });
+        });
+      }
+    };
+  });
 
-window.addEventListener('keyup', function (e) {
-  if(e.keyCode == 71){
-    socket.emit("up", myPerson);
-  }
-}, false);
-
-socket.on("clientdown", function(id){
-  ppl[id].key = true;
-  document.getElementById(id).className = "key pressed";
-});
-
-socket.on("clientup", function(id){
-  ppl[id].key = false;
-  document.getElementById(id).className = "key";
-});
+})();
