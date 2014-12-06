1db2ac948ebc329294a16929114e55ec669b2950
Max <themaxaxis@gmail.com>
2014-12-06 12:27:02 -0500

    add testing but it don't work

---
+++ public/src/testing.js
@@ -0,0 +1,23 @@
+
+var socket = io.connect('http://localhost:8888/');
+
+var ppl = {
+  1: {
+    key: false
+  },
+  2: {
+    key: false
+  },
+  3: {
+    key: false
+  },
+  4: {
+    key: false
+  }
+};
+
+window.addEventListener('keydown', function (e) {
+  if(e.keycode == 71){
+    alert("yo yo");
+  }
+}, false);
