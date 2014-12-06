e2326c32680da0152684d02170e12917eb7b6687
Nick <nick.hluska@gmail.com>
2014-12-06 18:10:39 -0500

    added tileset dependency

---
+++ public/assets/maps/Orthagonal.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Orthagonal" tilewidth="32" tileheight="32">
+ <tile id="0">
+  <image width="32" height="32" source="FinalTiles/BLCorner.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="FinalTiles/BRCorner.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="FinalTiles/Path.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="FinalTiles/PathLR.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="FinalTiles/ULCorner.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="FinalTiles/URCorner.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="FinalTiles/Green.png"/>
+ </tile>
+</tileset>
