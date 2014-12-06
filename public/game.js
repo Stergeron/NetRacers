ddf9fef2498067185268cd79ac8b2892a33e1d3a
William Tan <williamjefftan@gmail.com>
2014-12-06 12:07:42 -0500

    initial game.js

---
+++ public/game.js
@@ -0,0 +1,16 @@
+var Q = Quintus()
+    .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI")
+    .setup({ maximize: true })
+    .controls().touch()
+
+Q.Sprite.extend("Car",{ //Create car sprite
+
+    init: function(c) {
+
+        this._super(c, {
+            sheet: "car",
+            x: 410,
+            y: 90
+        })
+    }
+}
