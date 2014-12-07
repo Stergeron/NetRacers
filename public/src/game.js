8ebb1c31ca47005daa8d564100bc60046f609e48
Max <themaxaxis@gmail.com>
2014-12-07 16:50:29 -0500

    merge

--- public/src/game.js
+++ public/src/game.js
@@ -1,63 +1,62 @@
 var startMatch = function(url){
+  var Q = Quintus({ imagePath: "assets/images/",
+  audioPath: "assets/music/",
+  dataPath: "assets/maps/",
+  audioSupported: ["mp3"] })
+  .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, Audio, TMX")
+  .setup({ maximize: true })
+  .enableSound()
+  .controls(true).touch();
 
-    var Q = Quintus({ imagePath: "assets/images/",
-                      audioPath: "assets/music/",
-                      dataPath: "assets/maps/",
-                      audioSupported: ["mp3"] })
-        .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, Audio, TMX")
-        .setup({maximize: true})
-        .enableSound()
-        .controls(true).touch();
+  Q.gravityY = 0;
+  Q.gravityX = 0;
 
-        Q.gravityY = 0;
-        Q.gravityX = 0;
+  Q.input.touchControls({
+    controls:  [ ['left','<' ],
+    ['right','>' ],
+    ["up", "^"],
+    ["down", "V"],
+    ['fire', 'O' ]]
+  });
 
-      Q.input.touchControls({
-        controls:  [ ['left','<' ],
-        ['right','>' ],
-        ["up", "^"],
-        ["down", "V"],
-        ['fire', 'O' ]]
-      });
+  Q.component("carControls", {
 
-    Q.component("carControls", {
+    added: function() {
+      var p = this.entity.p;
 
-        added: function() {
-            var p = this.entity.p;
+      if(!p.stepDistance) { p.stepDistance = 16; }
+        if(!p.stepDelay) { p.stepDelay = 0.1; }
 
-            if(!p.stepDistance) { p.stepDistance = 16; }
-            if(!p.stepDelay) { p.stepDelay = 0.2; }
-
-            p.stepWait = 0;
-            this.entity.on("step",this,"step");
-            this.entity.on("hit", this,"collision");
-          },
+          p.stepWait = 0;
+          this.entity.on("step",this,"step");
+          this.entity.on("hit", this,"collision");
+        },
 
         collision: function(col) {
-            var p = this.entity.p;
+          var p = this.entity.p;
 
-            if(p.stepping) {
-                p.stepping = false;
-                p.x = p.origX;
-                p.y = p.origY;
-            }
+          if(p.stepping) {
+            p.stepping = false;
+            p.x = p.origX;
+            p.y = p.origY;
+          }
 
         },
 
         step: function(dt) {
-            var p = this.entity.p,
-            moved = false;
-            p.stepWait -= dt;
-            if(p.stepping) {
-                p.x += p.diffX * dt / p.stepDelay;
-                p.y += p.diffY * dt / p.stepDelay;
-                p.angle = p.angl
-            }
+          var p = this.entity.p,
+          moved = false;
+          p.stepWait -= dt;
+          if(p.stepping) {
+            p.x += p.diffX * dt / p.stepDelay;
+            p.y += p.diffY * dt / p.stepDelay;
+            p.angle = p.angl
+          }
 
-            if(p.stepWait > 0) { return; }
+          if(p.stepWait > 0) { return; }
             if(p.stepping) {
-                p.x = p.destX;
-                p.y = p.destY;
+              p.x = p.destX;
+              p.y = p.destY;
             }
             p.stepping = false;
 
@@ -65,60 +64,62 @@ var startMatch = function(url){
             p.diffY = 0;
 
             if(Q.inputs['left']) {
-                p.diffX = -p.stepDistance;
+              p.diffX = -p.stepDistance;
             } else if(Q.inputs['right']) {
-                p.diffX = p.stepDistance;
+              p.diffX = p.stepDistance;
             }
 
             if(Q.inputs['up']) {
-                p.diffY = -p.stepDistance;
+              p.diffY = -p.stepDistance;
             } else if(Q.inputs['down']) {
-                p.diffY = p.stepDistance;
+              p.diffY = p.stepDistance;
             }
 
             p.angl = 0;
             if(p.diffX < 0) {
-                p.angl = 90-Math.atan2(-p.diffY, p.diffX)*180/Math.PI;
+              p.angl = 90-Math.atan2(-p.diffY, p.diffX)*180/Math.PI;
             } else if(p.diffX > 0) {
-                p.angl = 90-Math.atan2(-p.diffY, p.diffX)*180/Math.PI;
+              p.angl = 90-Math.atan2(-p.diffY, p.diffX)*180/Math.PI;
             } else if(p.diffY > 0) {
-                p.angl = 180;
+              p.angl = 180;
             } else if(p.diffY < 0){
-                p.angl = 0;
+              p.angl = 0;
             }
 
             if(p.diffY || p.diffX ) {
-                p.stepping = true;
-                p.origX = p.x;
-                p.origY = p.y;
-                p.destX = p.x + p.diffX;
-                p.destY = p.y + p.diffY;
-                p.stepWait = p.stepDelay;
+              p.stepping = true;
+              p.origX = p.x;
+              p.origY = p.y;
+              p.destX = p.x + p.diffX;
+              p.destY = p.y + p.diffY;
+              p.stepWait = p.stepDelay;
             }
 
-        }
-    });
+          }
+        });
 
-    Q.Sprite.extend("Player",{ //Create car sprite
-        init: function(p) {
+        Q.Sprite.extend("Player",{ //Create car sprite
+          init: function(p) {
             this._super(p, {
-                asset: "car.png",
-                scale: 0.2
+              sheet: "car",
+              sprite: "car",
+              scale: .5,
+              collisionMask: Q.SPRITE_DEFAULT | Q.SPRITE_ACTIVE,
             });
             this.add('2d, carControls');
-        }
-    });
+          }
+        });
 
-    Q.Sprite.extend("Dummy", {
-        init: function(d) {
+        Q.Sprite.extend("Dummy", {
+          init: function(d) {
             this._super(d, {
-                asset: "car.png",
-                scale: 0.2
+              sheet: "car",
+              sprite: "car",
+              scale: .5
             });
             this.add('2d');
-        }
-    });
-
+          }
+        });
     Q.scene("level1", function(stage) {
         var socket = io.connect("/match");
         socket.emit("authenticate", {player: url[0], match: url[1]}, function(accept){
@@ -138,7 +139,9 @@ var startMatch = function(url){
         });
     });
 
-    Q.loadTMX(["car.png", "TinyCircle.tmx", "LevelTheme1.mp3"], function() {
-        Q.stageScene("level1");
+
+    Q.loadTMX(["car.png", "car.json", "TinyCircle.tmx", "LevelTheme1.mp3"], function() {
+      Q.compileSheets("car.png","car.json");
+      Q.stageScene("level1");
     });
 };
