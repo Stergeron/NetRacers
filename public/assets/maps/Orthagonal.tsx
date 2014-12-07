79b8ac76e733ee9af2386e70e8114875bf2f2251
William Tan <williamjefftan@gmail.com>
2014-12-06 19:53:47 -0500

    manually put in tilesets into the tmx files and correct image paths

--- public/assets/maps/Orthagonal.tsx
+++ public/assets/maps/Orthagonal.tsx
@@ -1,24 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Orthagonal" tilewidth="32" tileheight="32">
- <tile id="0">
-  <image width="32" height="32" source="FinalTiles/BLCorner.png"/>
- </tile>
- <tile id="1">
-  <image width="32" height="32" source="FinalTiles/BRCorner.png"/>
- </tile>
- <tile id="2">
-  <image width="32" height="32" source="FinalTiles/Path.png"/>
- </tile>
- <tile id="3">
-  <image width="32" height="32" source="FinalTiles/PathLR.png"/>
- </tile>
- <tile id="4">
-  <image width="32" height="32" source="FinalTiles/ULCorner.png"/>
- </tile>
- <tile id="5">
-  <image width="32" height="32" source="FinalTiles/URCorner.png"/>
- </tile>
- <tile id="6">
-  <image width="32" height="32" source="FinalTiles/Green.png"/>
- </tile>
+    <tile id="0">
+        <image width="32" height="32" source="../tiles/BLCorner.png"/>
+    </tile>
+    <tile id="1">
+        <image width="32" height="32" source="../tiles/BRCorner.png"/>
+    </tile>
+    <tile id="2">
+        <image width="32" height="32" source="../tiles/Path.png"/>
+    </tile>
+    <tile id="3">
+        <image width="32" height="32" source="../tiles/PathLR.png"/>
+    </tile>
+    <tile id="4">
+        <image width="32" height="32" source="../tiles/ULCorner.png"/>
+    </tile>
+    <tile id="5">
+        <image width="32" height="32" source="../tiles/URCorner.png"/>
+    </tile>
+    <tile id="6">
+        <image width="32" height="32" source="../tiles/Green.png"/>
+    </tile>
 </tileset>
